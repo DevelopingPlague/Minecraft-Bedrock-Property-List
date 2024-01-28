972a13c5f090c8ad5fdd73965346ef5ac623986c
DevelopingPlague <157980370+DevelopingPlague@users.noreply.github.com>
2024-01-28 18:06:41 +0100

    Official Download
    JS and TS are Provided

---
+++ playerProperties.ts
@@ -0,0 +1,39 @@
+import { Player } from "@minecraft/server";
+
+const PROPERTY = "target_properties"; // The Property Variable made to change the used DynamicProperty
+/**
+ * a Class used to manage custom Properties added via DynamicProperties into Minecraft
+ *
+     * requires a provided Target as Player Object and a Property as String
+     * Utilized DynamicPROPERTY for this Class is targetproperties
+    * @param {Player} Target
+    * @param {string} PROPERTY
+    */
+export class TargetProperty {
+    /**
+    * Constructor of the TargetProperty class
+    *
+    */
+    constructor() {
+    }
+    /** checks for a Property stored within the Player Object @param {Player} Target @param {string} Property */
+    hasProperty(Target : Player, Property: string) {
+        if (Property == undefined) { Target.setDynamicProperty(PROPERTY," ")}
+        return String(Target.getDynamicProperty(PROPERTY)).includes(String(Property).toLowerCase());
+    }
+    /** removes a Property from the Player Object @param {Player} Target @param {string} Property */
+    removeProperty(Target : Player, Property:string) {
+        if (Property == undefined) { Target.setDynamicProperty(PROPERTY," ")}
+        Target.setDynamicProperty(PROPERTY, String(Target.getDynamicProperty(PROPERTY)).replace(`\n${Property.toLowerCase()}`, ""));
+    }
+    /** Adds a Property to the Player Object @param {Player} Target @param {string} PROPERTY */
+    addProperty(Target: Player, Property:string) {
+        if (Property == undefined) { Target.setDynamicProperty(PROPERTY," ")}
+        let properties = Target.getDynamicProperty(PROPERTY);
+        Target.setDynamicProperty(PROPERTY, String(properties += `\n${Property}`).toLowerCase());
+    }
+    /** lists all Properties within a Player Object @param {Player} Target */
+    listProperties(Target:Player) {
+        return Target.getDynamicProperty(PROPERTY);
+    }
+}
